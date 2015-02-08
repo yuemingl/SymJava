0af7aaabe4e4b85bf567b15bfde9c3ce56f9767d
yueming <nkliuyueming@gmail.com>
2015-02-08 11:59:01 -0800

    add hard code solver example6

--- src/symjava/examples/Example6.java
+++ src/symjava/examples/Example6.java
@@ -53,6 +53,7 @@ public class Example6 {
 		diri.put(1, 0.0);
 		//solve(pde2, mesh2, diri, "patch_triangle.dat");
 		solve(pde2, mesh2, diri, "triangle.dat");
+		solve2(pde2, mesh2, diri, "triangle_hardcode.dat");
 		
 	}
 	
@@ -87,10 +88,6 @@ public class Example6 {
 		Expr ry = -jacMat[0][1]/jac; //ry = -xs/jac //bugfix missed a minus sign!
 		Expr sx = -jacMat[1][0]/jac; //sx = -yr/jac
 		Expr sy =  jacMat[0][0]/jac; //sy =  xr/jac
-//		Expr rx =  jacMat[1][1]; //rx =  ys/jac
-//		Expr ry =  jacMat[0][1]; //ry =  xs/jac
-//		Expr sx = -jacMat[1][0]; //sx = -yr/jac
-//		Expr sy =  jacMat[0][0]; //sy =  xr/jac
 		System.out.println(jac);
 		System.out.println(rx);
 		System.out.println(ry);
@@ -138,7 +135,8 @@ public class Example6 {
 					String.format("rhs%d",i),rhs*jac,new Expr[]{r,s}), tri);
 		}
 		
-//		Expr ttt = lhsInt[0][1].integrand.subs(x1, -1.996794872)
+//		Expr ttt = lhsInt[0][1].integrand
+//		.subs(x1, -1.996794872)
 //		.subs(x2, -1.786508761)
 //		.subs(x3, -1.381601534)
 //		.subs(y1,  1.996794872)
@@ -148,7 +146,6 @@ public class Example6 {
 //		Func fttt = new Func("fttt",ttt);
 //		BytecodeFunc bfttt = fttt.toBytecodeFunc();
 //		System.out.println(bfttt.apply());
-	
 		
 		//Generate bytecode for the integration
 		//You can save the class to some place for later use
@@ -175,8 +172,7 @@ public class Example6 {
 				for(int j=0; j<shapeFuns.length; j++) {
 					int idxJ = e.nodes.get(j).index-1;
 					double t = lhsNInt[i][j].eval(nodeCoords);
-					System.out.println(idxI+" "+idxJ+" "+t);
-					
+					//System.out.println(idxI+" "+idxJ+" "+t);
 					matA[idxI][idxJ] += t;
 				}
 				vecb[idxI] += rhsNInt[i].eval(nodeCoords);
@@ -196,12 +192,11 @@ public class Example6 {
 				}
 			}
 		}
-        for(int i=100; i<=110; i++) {
-        	for(int j=100; j<=110; j++)
-        		System.out.print(A.get(i-1, j-1)+" ");
-        	System.out.println();
-        }
-        
+//        for(int i=100; i<=110; i++) {
+//        	for(int j=100; j<=110; j++)
+//        		System.out.print(A.get(i-1, j-1)+" ");
+//        	System.out.println();
+//        }
 		Matrix x = A.solve(b);
 //		for(int i=0; i<x.getRowDimension(); i++)
 //			System.out.println(x.get(i, 0));
@@ -226,5 +221,133 @@ public class Example6 {
 				A.set(row, c, 0.0);
 			}
 		}
+	}	
+
+	/**
+	 * Hard code the solver for
+	 * 
+	 * dot(grad(u), grad(v)) == (-2*(x*x+y*y)+36)*v)
+	 * 
+	 * @param pde
+	 * @param mesh
+	 * @param dirichlet
+	 * @param output
+	 */
+	public static void solve2(Eq pde, Mesh2D mesh, Map<Integer, Double> dirichlet, String output) {
+		//Assemble the system
+		System.out.println("Start assemble the system...");
+		long begin = System.currentTimeMillis();
+		List<Domain> eles = mesh.getSubDomains();
+		double[][] matA = new double[mesh.nodes.size()][mesh.nodes.size()];
+		double[] vecb = new double[mesh.nodes.size()];
+		for(Domain d : eles) {
+			Element e = (Element)d;
+			double[] coords = e.getNodeCoords();
+			double x1 = coords[0];
+			double x2 = coords[1];
+			double x3 = coords[2];
+			double y1 = coords[3];
+			double y2 = coords[4];
+			double y3 = coords[5];
+			//x = x1*r+x2*s+x3*(1-r-s)
+			//y = y1*r+y2*s+y3*(1-r-s)
+			double xr = x1-x3;
+			double xs = x2-x3;
+			double yr = y1-y3;
+			double ys = y2-y3;
+			// jac = (xr xs)
+			//       (yr ys)
+			double jac = xr*ys-xs*yr;
+			double rx =  ys/jac;
+			double ry = -xs/jac;
+			double sx = -yr/jac;
+			double sy =  xr/jac;
+			double tx = -rx - sx;
+			double ty = -ry - sy;
+			
+			double[][] lhs = {
+					{rx*rx + ry*ry, rx*sx + ry*sy, rx*tx+ry*ty},
+					{sx*rx + sy*ry, sx*sx + sy*sy, sx*tx+sy*ty},
+					{tx*rx + ty*ry, tx*sx + ty*sy, tx*tx+ty*ty},
+			};
+			for(int i=0; i<3; i++) {
+				int idxI =  e.nodes.get(i).index-1;
+				for(int j=0; j<3; j++) {
+					int idxJ = e.nodes.get(j).index-1;
+					double t = lhs[i][j]*jac*0.5;
+					matA[idxI][idxJ] += t;
+				}
+			}
+
+			double[][] intPnW = {
+					{0.5, 0.5, 1.0/6.0},
+					{0.0, 0.5, 1.0/6.0},
+					{0.5, 0.0, 1.0/6.0}
+			};
+			for(int k=0; k<intPnW.length; k++) {
+				double r=intPnW[k][0];
+				double s=intPnW[k][1];
+				double t=1-r-s;
+				double w=intPnW[k][2];
+				double x = x1*r+x2*s+x3*t;
+				double y = y1*r+y2*s+y3*t;
+				//f=-2*(x*x+y*y)+36
+				double f = -2.0*(x*x+y*y)+36.0;
+				vecb[e.nodes.get(0).index - 1] += f*r*jac*w;
+				vecb[e.nodes.get(1).index - 1] += f*s*jac*w;
+				vecb[e.nodes.get(2).index - 1] += f*t*jac*w;
+			}
+
+		}
+		System.out.println("Assemble done! Time: "+(System.currentTimeMillis()-begin)+"ms");
+		
+		System.out.println("Solving...");
+		begin = System.currentTimeMillis();
+		Matrix A = new Matrix(matA);
+		Matrix b = new Matrix(vecb, vecb.length);
+		if(dirichlet != null) {
+			for(Node n : mesh.nodes) {
+				Double diri = dirichlet.get(n.type);
+				if(diri != null) {
+					setDirichlet(A, b, n.index-1, diri);
+				}
+			}
+		}
+//        for(int i=100; i<=110; i++) {
+//        	for(int j=100; j<=110; j++)
+//        		System.out.print(A.get(i-1, j-1)+" ");
+//        	System.out.println();
+//        }
+//		for(int i=100; i<=110; i++) {
+//			System.out.println(b.get(i-1, 0));
+//		}
+		/*
+3.678403159123969 -0.9973611181090696 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 
+-0.9973611181090696 3.877353541972138 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 
+0.0 0.0 3.525365130978284 -0.4486227282431723 -0.393018578918151 0.0 0.0 0.0 0.0 -0.24553903148664002 0.0 
+0.0 0.0 -0.4486227282431723 3.5372167175010087 -0.8194573851545361 0.0 0.0 0.0 0.0 0.0 0.0 
+0.0 0.0 -0.393018578918151 -0.8194573851545361 3.535701918507291 0.0 0.0 0.0 0.0 0.0 0.0 
+0.0 0.0 0.0 0.0 0.0 3.4945816438189032 -0.6132348151677102 -0.22191378338605314 0.0 0.0 0.0 
+0.0 0.0 0.0 0.0 0.0 -0.6132348151677102 3.707317244324667 -0.8671907304126691 0.0 0.0 0.0 
+0.0 0.0 0.0 0.0 0.0 -0.22191378338605314 -0.8671907304126691 3.4758034494659844 0.0 0.0 0.0 
+0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 3.623619648286562 0.0 0.0 
+0.0 0.0 -0.24553903148664002 0.0 0.0 0.0 0.0 0.0 0.0 3.501878694448894 -0.4577721650038804 
+0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 -0.4577721650038804 3.6326693760208175 
+5.920480649794939
+5.830543653385974
+11.166016717821526
+9.51298710892804
+7.987159886596924
+9.00295976201226
+6.471980478636926
+9.623214010483725
+6.664903619294702
+10.961199858227298
+7.935696861306976
+		 */
+		Matrix x = A.solve(b);
+		mesh.writeTechplot(output, x.getArray());
+		System.out.println("Solved! Time: "+(System.currentTimeMillis()-begin)+"ms");
+		System.out.println("See the output file(Tecplot format) "+output+" for the solution.");
 	}	
 }
