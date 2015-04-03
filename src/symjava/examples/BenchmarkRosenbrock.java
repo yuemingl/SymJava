19c8d3aa660fc3a096c5ff5f657f64c8b9376565
yuemingl <nkliuyueming@gmail.com>
2015-04-03 16:39:31 -0700

    bigfux for sorting symbols; add opt examples

--- src/symjava/examples/BenchmarkRosenbrock.java
+++ src/symjava/examples/BenchmarkRosenbrock.java
@@ -23,28 +23,42 @@ public class BenchmarkRosenbrock {
 		rosen  =  Sum.apply(100*pow(xi-xim1*xim1,2) + pow(1-xim1,2), i, 2, N);
 		System.out.println("Rosenbrock function with N="+N+": "+rosen);
 		
+		boolean debug = true;
+		
 		Expr[] freeVars = xi.get(1, N);
 		begin = System.currentTimeMillis();
 		SymVector grad = SymMath.grad(rosen);
-		end = System.currentTimeMillis();
-		System.out.println("Hessian Time: "+((end-begin)/1000.0));
 		SymMatrix hess = SymMath.hess(rosen);
+		end = System.currentTimeMillis();
+		System.out.println("Symbolic Time: "+((end-begin)/1000.0));
+		
+		begin = System.currentTimeMillis();
 		NumVector numGrad = grad.toNumVector(freeVars);
+		end = System.currentTimeMillis();
+		System.out.println("Gradient Compile Time: "+((end-begin)/1000.0));
+
+		begin = System.currentTimeMillis();
 		NumMatrix numHess = hess.toNumMatrix(freeVars);
+		end = System.currentTimeMillis();
+		System.out.println("Hessian Compile Time: "+((end-begin)/1000.0));
 
 		double[] args = new double[freeVars.length];
-		System.out.println(grad);
 		double[] gradResult = numGrad.eval(args);
-		for(double d : gradResult)
-			System.out.println(d);
-		System.out.println();
-
-		System.out.println(hess);
-		double[][] hessResult = numHess.eval(args);
-		for(double[] row : hessResult) {
-			for(double d : row)
-				System.out.print(d+" ");
+		if(debug) {
+			System.out.println(grad);
+			for(double d : gradResult)
+				System.out.println(d);
 			System.out.println();
+		}
+		
+		double[][] hessResult = numHess.eval(args);
+		if(debug) {
+			System.out.println(hess);
+			for(double[] row : hessResult) {
+				for(double d : row)
+					System.out.print(d+" ");
+				System.out.println();
+			}
 		}
 		
 		int NN = 100;
--- src/symjava/examples/OptimizationExamples.java
+++ src/symjava/examples/OptimizationExamples.java
@@ -7,15 +7,25 @@ import symjava.symbolic.Symbol;
 import symjava.symbolic.Symbols;
 import symjava.symbolic.utils.AddList;
 import static symjava.math.SymMath.*;
-import static symjava.symbolic.Symbol.C0;
+import static symjava.symbolic.Symbol.*;
 
+/**
+ * http://docs.scipy.org/doc/scipy/reference/tutorial/optimize.html
+ * @author yliu
+ *
+ */
 public class OptimizationExamples {
 
 	public static void main(String[] args) {
-		test1();
-		test2();
+		//test1();
+		//test2();
+		test3_constrained();
+		test3_unconstrained();
 	}
 	
+	/**
+	 * Newton-Conjugate-Gradient algorithm (method='Newton-CG')
+	 */
 	public static void test1() {
 		//Rosenbrock function of N variables
 		int N = 5;
@@ -37,6 +47,9 @@ public class OptimizationExamples {
 		//[ 1.  1.  1.  1.  1.]
 	}
 
+	/**
+	 * Newton-Conjugate-Gradient algorithm (method='Newton-CG')
+	 */
 	public static void test2() {
 		//Another way for the definition of Rosenbrock function
 		int N = 5;
@@ -55,5 +68,37 @@ public class OptimizationExamples {
 		NewtonOptimization.solve(eq, x0, 1000, 1e-6, false);
 		//[ 1.  1.  1.  1.  1.]
 	}
+	
+	/**
+	 * Constrained minimization of multivariate scalar functions (minimize)
+	 */
+	public static void test3_constrained() {
+		Expr obj = 2*x*y + 2*x - x*x - 2*y*y;
+		Symbol lmd1 = new Symbol("\\lambda_1");
+		Symbol lmd2 = new Symbol("\\lambda_2");
+		Symbol c = new Symbol("c");
+		
+		Expr L = obj + lmd1*(pow(x,3)-y) + lmd2*(y-1-c*c);
+		Expr[] freeVars = new Expr[]{x, y, lmd1, lmd2, c};
+		//double[] x0 = {-1.0,1.0, 0, 0, 0}; //stuck?
+		double[] x0 = {0.0,0.0, 0, 0, 0}; //works
+		Eq eq = new Eq(L, C0, freeVars);
+		System.out.println(eq);
 
+		NewtonOptimization.solve(eq, x0, 1000, 1e-6, false);
+		//[ 1.00000009  1.        ]
+	}
+	
+	public static void test3_unconstrained() {
+		Expr obj = 2*x*y + 2*x - x*x - 2*y*y;
+		
+		Expr L = obj;
+		Expr[] freeVars = new Expr[]{x, y};
+		double[] x0 = {-1.0,1.0};
+		Eq eq = new Eq(L, C0, freeVars);
+		System.out.println(eq);
+
+		NewtonOptimization.solve(eq, x0, 1000, 1e-6, false);
+		//[ 2.  1.]
+	}
 }
