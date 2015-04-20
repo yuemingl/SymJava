d1fa55508eae808fc0a58d6834be2999af862972
yuemingl <nkliuyueming@gmail.com>
2015-04-20 10:10:32 -0700

    add some comments

--- src/lambdacloud/core/CloudVar.java
+++ src/lambdacloud/core/CloudVar.java
@@ -2,7 +2,6 @@ package lambdacloud.core;
 
 import io.netty.channel.Channel;
 import lambdacloud.net.CloudQuery;
-import lambdacloud.net.CloudResp;
 import lambdacloud.net.CloudVarHandler;
 import lambdacloud.net.CloudVarRespHandler;
 import lambdacloud.net.CloudVarResp;
@@ -13,6 +12,24 @@ import symjava.symbolic.Symbol;
 import symjava.symbolic.utils.JIT;
 import symjava.symbolic.utils.Utils;
 
+/**
+ * An instance of CloudVar represents a variable on the cloud server.
+ * The variable can be created on local and stored to cloud side.
+ * For example:
+ * <p><blockquote><pre>
+ *     CloudVar var = new CloudVar("myvar").init(new double[]{1, 2, 3, 4, 5});
+ *     var.sotoreToCloud();
+ * </pre></blockquote>
+ * <P>A variable on the cloud can be fetched to local by providing the correct name.
+ * <p><blockquote><pre>
+ *     if(var.fetchToLocal()) {
+ *       for(double d : var.getData()) {
+ *         System.out.println(d);
+ *       }
+ *     }
+ * </pre></blockquote>
+ *
+ */
 public class CloudVar extends Symbol {
 	double[] data = new double[0];
 	boolean isOnCloud = false;
@@ -47,15 +64,44 @@ public class CloudVar extends Symbol {
 		return this;
 	}
 
+	/**
+	 * Initialize the cloud variable with the given array.
+	 * The new cloud variable simply wrap the array; that is,
+	 * it is backed by the given array. Any modifications to the 
+	 * cloud variable will cause the array to be modified and vice versa.
+	 * @param array
+	 * @return
+	 */
 	public CloudVar init(double ...array) {
 		this.data = array;
 		return this;
 	}
 
+	/**
+	 * Set the value of the backed array at index
+	 * @param index
+	 * @param value
+	 */
 	public void set(int index, double value) {
 		data[index] = value;
 	}
 	
+	/**
+	 * Get the value of the backed array at index
+	 * @param index
+	 * @return
+	 */
+	public double get(int index) {
+		return data[index];
+	}
+	 
+	/**
+	 * Resize the backed array. Old data will be copied to the new backed array
+	 * if the new size is larger than the old size otherwise the data that beyond
+	 * the new size will be discarded.
+	 * @param size
+	 * @return
+	 */
 	public CloudVar resize(int size) {
 		if(this.data == null)
 			this.data = new double[size];
@@ -71,14 +117,44 @@ public class CloudVar extends Symbol {
 		return this;
 	}
 	
+	/**
+	 * Return the length of the backed array
+	 * @return
+	 */
+	public int size() {
+		return data.length;
+	}
+	
+	/**
+	 * Return the length of the backed array
+	 * @return
+	 */
+	public int length() {
+		return data.length;
+	}
+	
+	/**
+	 * Return the name of the cloud variable. The name is the identifier
+	 * of the cloud variable on the cloud server. Any local instance of 
+	 * CloudVar has the same name will be assumed to be the same vaiable 
+	 * on the cloud side.
+	 * @return
+	 */
 	public String getName() {
 		return this.label;
 	}
 	
+	/**
+	 * Return the backed array
+	 * @return
+	 */
 	public double[] getData() {
 		return data;
 	}
 	
+	/**
+	 * Store the local variable to the cloud. 
+	 */
 	public void storeToCloud() {
 		CloudClient client = CloudConfig.getClient();
 		CloudVarRespHandler handler = client.getCloudVarRespHandler();
@@ -95,7 +171,8 @@ public class CloudVar extends Symbol {
 	}
 	
 	/**
-	 * Fetch a cloud variable to local. Return true if success. 
+	 * Fetch a cloud variable to local. The name of the variable 
+	 * on the cloud must be specified. Return true if success.
 	 * Call getData() to access the data in the cloud variable
 	 * @return
 	 */
@@ -127,10 +204,6 @@ public class CloudVar extends Symbol {
 	
 	public void setOnCloudFlag(boolean flag) {
 		this.isOnCloud = flag;
-	}
-	
-	public int size() {
-		return data.length;
 	}
 	
 	@Override
