c73f5a1c583b02650176970ffed47802d0be7bd0
yuemingl <nkliuyueming@gmail.com>
2015-06-17 16:00:51 -0700

    Add bytecodeGen()

--- src/symjava/symbolic/Cos.java
+++ src/symjava/symbolic/Cos.java
@@ -1,7 +1,17 @@
 package symjava.symbolic;
 
+import java.util.Map;
+
 import symjava.symbolic.arity.UnaryOp;
 import symjava.symbolic.utils.Utils;
+
+import com.sun.org.apache.bcel.internal.Constants;
+import com.sun.org.apache.bcel.internal.generic.ConstantPoolGen;
+import com.sun.org.apache.bcel.internal.generic.InstructionFactory;
+import com.sun.org.apache.bcel.internal.generic.InstructionHandle;
+import com.sun.org.apache.bcel.internal.generic.InstructionList;
+import com.sun.org.apache.bcel.internal.generic.MethodGen;
+import com.sun.org.apache.bcel.internal.generic.Type;
 
 public class Cos extends UnaryOp {
 	public Cos(Expr arg) {
@@ -31,5 +41,17 @@ public class Cos extends UnaryOp {
 		}
 		return false;
 	}
-
+	
+	@Override
+	public InstructionHandle bytecodeGen(String clsName, MethodGen mg,
+			ConstantPoolGen cp, InstructionFactory factory,
+			InstructionList il, Map<String, Integer> argsMap, int argsStartPos, 
+			Map<Expr, Integer> funcRefsMap) {
+		InstructionHandle startPos = arg.bytecodeGen(clsName, mg, cp, factory, il, argsMap, argsStartPos, funcRefsMap);
+		il.append(factory.createInvoke("java.lang.Math", "cos",
+				Type.DOUBLE, 
+				new Type[] { Type.DOUBLE },
+		Constants.INVOKESTATIC));
+		return startPos;
+	}
 }
