5eb4b36ae7937d91c92ce1b93b85fecf305a094c
yuemingl <nkliuyueming@gmail.com>
2015-06-30 22:25:29 -0700

    Add config and benchmark files

--- src/lambdacloud/examples/ExampleMonteCarlo.java
+++ src/lambdacloud/examples/ExampleMonteCarlo.java
@@ -165,8 +165,8 @@ public class ExampleMonteCarlo {
 	
 	public static void main(String[] args) {
 		System.out.println("Current working dir="+System.getProperty("user.dir"));
-		int N = 10000000;
-		String configFile = "job_vm2.conf";
+		int N = 1000000;
+		String configFile = "job_google.conf";
 		boolean isAsync = false;
 		if(args.length == 3) {
 			N = Integer.valueOf(args[0]);
