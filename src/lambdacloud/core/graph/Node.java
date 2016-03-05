2a74d17b0b711e3d39c539542a2b580a41bc3b49
yuemingl <nkliuyueming@gmail.com>
2016-03-05 13:22:25 -0800

    add comment to Node; children is a map.

--- src/lambdacloud/core/graph/Node.java
+++ src/lambdacloud/core/graph/Node.java
@@ -12,13 +12,17 @@ import symjava.symbolic.Expr;
 
 public class Node {
 	public Expr expr;
-	public List<Expr> args = new ArrayList<Expr>();
-	public BytecodeFunc func;
-	public CloudFunc cfunc;
-	public Map<String, Node> children = new HashMap<String, Node>();
+	public List<Expr> args = new ArrayList<Expr>(); //arguments of expr; added at compile time
+	
+	public BytecodeFunc func; //for test purpose
+	
+	public CloudFunc cfunc; // All the server info are stored in CloudFunc
+	public Map<String, Node> children = new HashMap<String, Node>(); //Map of the child nodes if an argument is a node
+	
 	public boolean isDevice() {
 		return expr.getDevice()!=null;
 	}
+	
 	public Node() {
 		
 	}
