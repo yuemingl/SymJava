f07572ff6c68fe1745b3e8646f4eab01667ca81c
yuemingl <nkliuyueming@gmail.com>
2016-03-05 13:16:22 -0800

    Need further consideration for the fucntion CPU and GPU

--- src/lambdacloud/core/LambdaCloud.java
+++ src/lambdacloud/core/LambdaCloud.java
@@ -4,10 +4,15 @@ import lambdacloud.core.lang.LCDevice;
 import symjava.symbolic.Expr;
 
 public class LambdaCloud {
+	//Just for test
+	public static int maxNumCPU = 3;
+	public static int currentCPU = 0;
 	public static Expr CPU(Expr expr) {
-		expr.runOn(new LCDevice("/cpu"));
+		expr.runOn(new LCDevice(String.valueOf(currentCPU%maxNumCPU)));
+		currentCPU++;//round robin
 		return expr;
 	}
+	
 	public static Expr GPU(Expr expr) {
 		expr.runOn(new LCDevice("/gpu"));
 		return expr;
