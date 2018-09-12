25a6301eaf3ff22a8745e7feadf9fb4d9faab5f0
yuemingl <nkliuyueming@gmail.com>
2018-09-11 17:45:17 -0700

    Another WiFi password

--- src/symjava/examples/WiFiPassword.java
+++ src/symjava/examples/WiFiPassword.java
@@ -1,7 +1,6 @@
 package symjava.examples;
 
-import static symjava.math.SymMath.pow;
-import static symjava.math.SymMath.sin;
+import static symjava.math.SymMath.*;
 import static symjava.symbolic.Symbol.x;
 import symjava.bytecode.BytecodeFunc;
 import symjava.domains.Domain;
@@ -14,15 +13,32 @@ public class WiFiPassword {
 	public static void password() {
 		//Define the interval
 		Domain I = Interval.apply(-5, 5).setStepSize(0.01);
+		
 		//Define the integral
 		Expr f = Integrate.apply(
 				(pow(x,3)*pow(sin(x),2))/(pow(x,4)+2*pow(x,2)+1), I);
 		System.out.println(f);
+		
 		//Compile f to perform numerical integration
 		BytecodeFunc cf = JIT.compile(f);
 		System.out.println(String.format("%.3f", cf.apply()));
 	}
+	
+	public static void password2() {
+		// Define the interal
+		Domain I = Interval.apply(-2, 2).setStepSize(0.00001);
+
+		// Define the integral
+		Expr i = Integrate.apply((pow(x, 3) * cos(x / 2.0) + 0.5)
+				* sqrt(4 - x * x), I);
+		System.out.println(i);
+
+		// Compile the integral to perform numerical integration
+		BytecodeFunc f = JIT.compile(i);
+		System.out.println(f.apply());
+	}
 	public static void main(String[] args) {
-		password();
+		//password();
+		password2();
 	}
 }
